08feb1af82816e01acbe080ccdd89525abc557fb
Adi Andrea <dekz.itfz@gmail.com>
2016-02-06 19:59:02 +0800

    adding volley lib + integration part 1

--- app/src/main/java/id/dekz/code/ce/fragment/FragmentConvert.java
+++ app/src/main/java/id/dekz/code/ce/fragment/FragmentConvert.java
@@ -2,6 +2,7 @@ package id.dekz.code.ce.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +14,25 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.txusballesteros.AutoscaleEditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 import id.dekz.code.ce.R;
+import id.dekz.code.ce.pojo.MySingleton;
+import id.dekz.code.ce.task.ConvertTask;
 
 /**
  * Created by DEKZ on 2/6/2016.
@@ -23,6 +40,15 @@ import id.dekz.code.ce.R;
 public class FragmentConvert extends Fragment {
     private View rootView;
     private AutoscaleEditText etAmount;
+
+    private TextView cFrom;
+    private TextView cTo;
+    private TextView tvResultConvert;
+    private TextView tvCurrentRate;
+    private TextView tvCRDate;
+    private SimpleDateFormat simpleDateFormat;
+
+    private long rate;
 
     public FragmentConvert() {
         // Required empty public constructor
@@ -37,6 +63,12 @@ public class FragmentConvert extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_convert, container, false);
+
+        cFrom = (TextView) rootView.findViewById(R.id.tvCurrencyFrom);
+        cTo = (TextView) rootView.findViewById(R.id.tvCurrencyTo);
+        tvResultConvert = (TextView) rootView.findViewById(R.id.tvResultConvert);
+        tvCurrentRate = (TextView) rootView.findViewById(R.id.tvCurrentRate);
+        tvCRDate = (TextView) rootView.findViewById(R.id.tvCRDate);
 
         etAmount = (AutoscaleEditText) rootView.findViewById(R.id.etAmount);
 
@@ -54,11 +86,18 @@ public class FragmentConvert extends Fragment {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
-                    Toast.makeText(getActivity(), "convert!", Toast.LENGTH_SHORT).show();
+                    String base = cFrom.getText().toString();
+                    String symbol = cTo.getText().toString();
+                    long amountBase = Long.parseLong(etAmount.getText().toString());
+                    getSingleRate(base,symbol,amountBase);
                 }
                 return false;
             }
         });
+
+        Calendar c = Calendar.getInstance();
+        String pattern = "MM, DD yyyy";
+        simpleDateFormat = new SimpleDateFormat(pattern, new Locale("en", "US"));
 
         return rootView;
     }
@@ -66,6 +105,78 @@ public class FragmentConvert extends Fragment {
     @Override
     public void onResume(){
         super.onResume();
-        Toast.makeText(getActivity(), "resumed!", Toast.LENGTH_SHORT).show();
+        refreshCurrentRate(cFrom.getText().toString(),cTo.getText().toString());
+    }
+
+    private void refreshCurrentRate(String base, final String symbols){
+        String  URLsingleRate = "http://api.fixer.io/latest?base="+base+"&symbols="+symbols;
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                URLsingleRate, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                //Log.d(TAG, response.toString());
+                Date date = null;
+                try {
+                    String datestr = response.getString("date");
+
+                    //date = simpleDateFormat.parse(datestr);
+                    //Toast.makeText(getActivity(),""+date,Toast.LENGTH_SHORT).show();
+                    tvCRDate.setText(datestr);
+
+                    JSONObject rates = response.getJSONObject("rates");
+                    String cRate = rates.getString(symbols);
+
+                    tvCurrentRate.setText(cRate);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(),"Error: " + e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(),
+                        "error response", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MySingleton.getInstance(getActivity()).addToRequestQueue(jsonObjReq);
+    }
+
+    private void getSingleRate(String base, final String symbols, final long amountBase) {
+        String  URLsingleRate = "http://api.fixer.io/latest?base="+base+"&symbols="+symbols;
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                URLsingleRate, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                //Log.d(TAG, response.toString());
+
+                try {
+                    JSONObject rates = response.getJSONObject("rates");
+                    //rate = Long.parseLong(rates.getString(symbols));
+                    rate = (long)Double.parseDouble(rates.getString(symbols));
+                    long result = amountBase * rate;
+                    tvResultConvert.setText(String.valueOf(result));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(),"Error: " + e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getActivity(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MySingleton.getInstance(getActivity()).addToRequestQueue(jsonObjReq);
     }
 }
